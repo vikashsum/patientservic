eee1189643c371f761755055d88c6182b6fe21bd
vikash <vikashsuman886@gmail.com>
2026-06-24 16:55:59 +0530

    Enable SSL for PostgreSQL connection

--- src/models/index.js
+++ src/models/index.js
@@ -8,23 +8,26 @@ const sequelize = new Sequelize(
   config.database.username,
   config.database.password,
   {
-  host: config.database.host,
-  port: config.database.port,
-  dialect: config.database.dialect,
+    host: config.database.host,
+    port: config.database.port,
+    dialect: config.database.dialect,
 
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+
     logging: config.app.nodeEnv === 'development' ? console.log : false,
+
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
     },
+
     define: {
       timestamps: true,
       underscored: true
